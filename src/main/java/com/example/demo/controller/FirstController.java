315de7c5080bb70e01b7b30ca7ce06800949bd39
rialda <rialda.spahic@gmail.com>
2018-03-15 20:20:06 +0100

    Landing page
    Created view and landing page

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
 	
-	@RequestMapping("/")
+	@ResponseBody
+	@RequestMapping("/home")
 	public String welcome() {
 		return "Hello World!";
 	}
 	
+	@RequestMapping("/index")
+	public String landingPage() {
+		return "landing_page";
+	}
 	 
 }
